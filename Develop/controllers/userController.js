8d09451d19d951c5e27feea13d59e1b049caff81
Change Agent <keasiley@icloud.com>
2023-05-28 16:41:32 -0500

    userController created

---
+++ Develop/controllers/userController.js
@@ -0,0 +1,67 @@
+const {User} = require('./models');
+
+module.exports = {
+    // Get all users
+    async getUsers(req, res) {
+        try {
+            const users = await User.findAll();
+            return res.json(users);
+            
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    },
+    // Get user by id
+    async getUserById(req, res) {
+        try {
+            const user = await User.findByPk(req.params.id);
+            return res.json(user);
+            
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    },
+    // Create user
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            return res.json(user);
+            
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    },
+    // Update user by id
+    async updateUser(req, res) {
+        try {
+            const user = await User.update(req.body, {
+                where: {
+                    id: req.params.id
+                }
+            });
+            return res.json(user);
+            
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    },
+    // Delete user by id
+    async deleteUser(req, res) {
+        try {
+            const user = await User.destroy({
+                where: {
+                    id: req.params.id
+                }
+            });
+            return res.json(user);
+            
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+    }
+}
