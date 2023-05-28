c04872430660bce455ac26f593bd467661d0a897
Change Agent <keasiley@icloud.com>
2023-05-28 15:17:22 -0500

    Index.js, Reaction.js,Thought.js, and User.js created in the Models folder

---
+++ Develop/models/Thought.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const reactionIdSchema = require('./ReactionId');
+
+const thoughtTextSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280
+        },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+        },
+    reactions: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Reaction'
+        },
+    ]
+    },
+);
+
+thoughtText
+  .virtual('reactionCount')
+  .get(function() {
+        return this.reactions.length;
+    });
+
+const Thought = model('thought', thoughtTextSchema);
+
+module.exports = Thought;
---
+++ Develop/models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trimmed: true
+        },
+    email: {
+        type: String,
+        required: true,
+        match: /.+\@.+\..+/,
+        unique: true
+        },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+        ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+    },
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
