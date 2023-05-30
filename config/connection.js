e36654438fc6e36a4877a1f374d7be3916b61eda
Change Agent <keasiley@icloud.com>
2023-05-30 13:18:59 -0500

    connection.js edited with correct DC name. API index edited to correct a typo

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,13 @@ const { connect, connection } = require('mongoose');
 
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/userandthoughtsDB';
 
 connect(connectionString);
 
 module.exports = connection;
+// const { connect, connection } = require('mongoose');
+
+// connect('mongodb://127.0.0.1:27017/userandthoughtsDB');
+
+// module.exports = connection;
